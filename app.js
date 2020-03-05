13cf75157cd3f6ad244c46a7c6531e1b7c590128
geovonnie vasquez <geovonnievasquez@gmail.com>
2020-03-04 23:01:51 -0600

    all done, runing into issues running the test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const idArray = []
 function appMenu(){
 //intro 
 console.log("Build your team!")
-//every team must have a manager
+//every team must have a manager so no need to use switch
 function createManager(){
 inquirer.prompt([
 {
@@ -144,40 +144,23 @@ function createTeam(){
         //end ai
     }
 
-    //write the team to output dir
-    fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
+    function buildTeam(){
 
+        if(!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdir(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath,render(teamMembers), "utf-8")
+     
+    }
+
+    createManager()
 };
+
 
 
 
 //end appMenu
 };
 
-
+appMenu()
 ​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
-
-
-//will all have name,id,and email
